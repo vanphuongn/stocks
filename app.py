c914260663882c36fd9933befa909fc4f3f3868d
vietthangk60 <vietthangk54@gmail.com>
2025-06-16 11:37:49 +0700

    add os

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import yfinance as yf
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -89,4 +90,5 @@ def export_stock(symbol):
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
